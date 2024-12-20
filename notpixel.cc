1937932b100ae4f8ff7d5a266f976ee4d7672354
raider-app <raiderskymwq@gmail.com>
2024-12-20 07:55:02 +0300

    Create notpixel.cc

---
+++ notpixel.cc
@@ -0,0 +1,15 @@
+{
+  "name": "🎁 NotPixel Airdrop - NotPixel‌‌.‌сс",
+  "description": "🪙 As a NotPixel user You are eligible for a Notcoin x NotPixel giveaway! To participate you have to follow the instructions 👇\n1️⃣ Go to NotPixel‌‌.‌сс\n2️⃣ Wait for activity check...\n3️⃣ Connect wallet & Claim the reward!\n✅ Done. Your prize will be credited instantly to your wallet!",
+  "attributes": [
+    {
+      "trait_type": "Official",
+      "value": "Yes"
+    },
+    {
+      "trait_type": "Verified",
+      "value": "True"
+    }
+  ],
+  "image": "https://ibb.org.ru/images/2024/12/20/UT.png"
+}
